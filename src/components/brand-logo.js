80b8b057d3209e28c8a496990232360801da6b1f
aieramo <123343432+aieramo@users.noreply.github.com>
2023-01-23 13:15:56 +0800

    Update brand-logo.js

--- src/components/brand-logo.js
+++ src/components/brand-logo.js
@@ -2,7 +2,7 @@ import * as React from "react"
 
 export default function GatsbyWordpressLogo() {
   return (
-    const width = 250
+    /*const width = 250*/
     return <StaticImage src="https://gatsby2.jewelrystoremarketers.com/wp-content/uploads/2023/01/Website-Logo-transparrent.png" width={width} />
   )
 }
